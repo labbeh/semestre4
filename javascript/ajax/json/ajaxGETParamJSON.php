c026b580abb8a9c2c1c3b715b0bfd2a41014ded5
labbeh <hugo.labbe@etu.univ-lehavre.fr>
2019-03-07 11:56:27 +0100

    transfere depuis public_html exo1 tp ajax

---
+++ javascript/ajax/json/ajaxGETParamJSON.php
@@ -0,0 +1,30 @@
+<?PHP
+
+	function test_input($data) {
+		 $data = trim($data);
+		 $data = stripslashes($data);
+		 $data = htmlentities($data);
+		 return $data;
+	}
+
+	$objbase = '{"val":0,"carre":0,"cube":0}';
+
+	if (isset($_GET['nb'])) {
+		 $nb = $_GET['nb'];
+		 if (empty($nb)) {
+		  	$obj = $objbase;
+		 }
+		 else {
+			  $nb = test_input($nb);
+			  //echo $nb."<br/>";
+			  $carre = $nb * $nb;
+			  $cube = $carre*$nb;
+			  $obj = '{"val":'.$nb.',"carre":'.$carre.',"cube":'.$cube.'}';
+		 }
+	}
+	else {
+	 	$obj = $objbase;
+	}
+
+	echo $obj;
+?>
